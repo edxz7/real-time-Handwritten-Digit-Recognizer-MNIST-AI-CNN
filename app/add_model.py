d4afc61c8958986f3bb3f5bdd10f973d34ecb25c
EChColorado <edxz7c@gmail.com>
2019-05-17 06:30:44 -0500

    Use pathlib to manage the paths in add_model.py

--- app/add_model.py
+++ app/add_model.py
@@ -4,11 +4,13 @@ Author: Eduardo Ch. Colorado
 E-mail: edxz7c@gmail.com
 """
 import sys
+from pathlib import Path
 # Available NET Architectures: resnet-ish, lenet-like, capsule_net
 model_name = sys.argv[1]
 model_url  = sys.argv[2]
+path = Path(__file__).parent
 
-with open("app/avaible_models.txt", "a") as f:
+with open(path/"avaible_models.txt", "a") as f:
     f.write("{} {}".format(model_name, model_url) + "\n")
 
 
