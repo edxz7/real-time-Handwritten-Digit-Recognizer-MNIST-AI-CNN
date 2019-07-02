eab512a4495893f3442e0b6477afc0200f35111d
Eduardo Ch. Colorado <edxz7c@gmail.com>
2019-07-01 21:19:01 -0500

    Revert "Ui Update"

--- app/static/canvas.js
+++ app/static/canvas.js
@@ -10,16 +10,16 @@ window.addEventListener("load", () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.fillRect(0, 0, canvas.width, canvas.height);
         // ctx.renderAll();
-        updateChart(zeros); // each time we cleared the canvas we reset chart
-        $("#status").removeClass(); // and remove the status icon
-
+        updateChart(zeros);          // each time we cleared the canvas we reset chart
+        $("#status").removeClass();  // and remove the status icon
+        
     }, false);
 
     /** CONSTANTS **/
     const CANVAS_BACKGROUND_COLOUR = "black" //"#1d252c";       
-
+    
     // Get the canvas element
-    const canvas = document.querySelector('#canvas'); // on querySelector search for the tag canvas in the index.html 
+    const canvas = document.querySelector('#canvas');  // on querySelector search for the tag canvas in the index.html 
     // Return a two dimensional drawing context
     // In canvas all the drawinng is performed in the context
     const ctx = canvas.getContext('2d');
@@ -32,25 +32,25 @@ window.addEventListener("load", () => {
     let maxWidth = 35
     let minWidth = 15
     let widthDelta = 0.25
-    let lx, ly = [0, 0]
-    let hue = 0 // if the hue strockeStyle is enabled
+    let lx, ly = [0,0]
+    let hue = 0                      // if the hue strockeStyle is enabled
     ctx.strokeStyle = 'white';
     ctx.lineJoin = 'round';
     ctx.lineCap = 'round';
     ctx.lineWidth = minWidth;
 
-    function draw(e) {
-        if (!isDrawing) return;
+    function draw(e){
+        if(!isDrawing) return;
         ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
         hue = ++hue % 360;
         ctx.beginPath()
-        ctx.moveTo(lx, ly)
+        ctx.moveTo(lx,ly)
         ctx.lineTo(e.offsetX, e.offsetY);
         ctx.stroke();
-        [lx, ly] = [e.offsetX, e.offsetY]
+        [lx,ly] = [e.offsetX, e.offsetY]
 
         ctx.lineWidth += widthDelta
-        if (ctx.lineWidth < minWidth || ctx.lineWidth > maxWidth) {
+        if(ctx.lineWidth < minWidth || ctx.lineWidth > maxWidth){
             widthDelta = -widthDelta
         }
     }
@@ -61,25 +61,12 @@ window.addEventListener("load", () => {
         isDrawing = true
         ctx.moveTo(e.offsetX, e.offsetY)
         ctx.closePath(); //close if previously open
-        [lx, ly] = [e.offsetX, e.offsetY]
+        [lx,ly] = [e.offsetX, e.offsetY]
     });
     canvas.addEventListener('mouseup', () => {
-        isDrawing = false
+        isDrawing = false 
         hue = 0
     });
     canvas.addEventListener('mouseout', () => isDrawing = false);
 
 });
-
-const canvas = document.querySelector('canvas');
-
-function fitToContainer(canvas) {
-    // Make it visually fill the positioned parent
-    canvas.style.width = '100%';
-    canvas.style.height = '75vh';
-    // ...then set the internal size to match
-    canvas.width = canvas.offsetWidth;
-    canvas.height = canvas.offsetHeight;
-}
-
-fitToContainer(canvas);
